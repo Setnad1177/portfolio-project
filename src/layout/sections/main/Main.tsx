584363daad742a5b93f0acf427d0d7ac0993bd68
Andrew <abrakadabro17@gmail.com>
2024-06-25 18:05:50 +0200

    main

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from "styled-components";
+import photo from '../../../assets/images/photo.webp'
+import {FlexWrapper} from "../../../components/FlexWrapper";
+
+export const Main = () => {
+    return (
+        <StyledMain>
+            <FlexWrapper align={"center"} justify={"space-around"}>
+                <div>
+                    <span>Hi There </span>
+                    <h2>I am Andrei Kondush</h2>
+                    <h1>The Software Developer.</h1>
+                </div>
+
+                <Photo src={photo} alt=""/>
+            </FlexWrapper>
+        </StyledMain>
+    );
+};
+
+const StyledMain = styled.div`
+min-height: 100vh;
+background-color: #fff5e7;
+`
+
+const Photo = styled.img`
+width: 350px;
+height: 430px;
+object-fit:cover;
+`
