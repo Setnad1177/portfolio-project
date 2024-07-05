3c1ebe3a5257252d1e250a32902777bf793a2520
Andrew <abrakadabro17@gmail.com>
2024-07-05 20:12:04 +0200

    testimony styling

--- src/layout/sections/testimony/Testimony.tsx
+++ src/layout/sections/testimony/Testimony.tsx
@@ -4,13 +4,16 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Icon} from "../../../components/icon/Icon";
 import {Slider} from "../../../components/slider/Slider";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {IconWrapper} from "../skills/skill/Skill";
 
 export const Testimony = () => {
     return (
         <StyledTestimony>
             <SectionTitle>Testimony</SectionTitle>
             <FlexWrapper direction={"column"} align={"center"}>
+                <IconWrapper>
                 <Icon iconId={"quoteAltRight"}/>
+                </IconWrapper>
                 <Slider/>
             </FlexWrapper>
         </StyledTestimony>
@@ -20,4 +23,8 @@ export const Testimony = () => {
 const StyledTestimony = styled.section`
     background-color: lightblue;
     min-height: 50vh;
+    
+    ${IconWrapper} {
+        margin: 40px 0 72px;
+    }
 `
