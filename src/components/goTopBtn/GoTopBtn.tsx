f93d529001b0862215866db614a738238e0cfa53
Andrew <abrakadabro17@gmail.com>
2024-07-19 11:35:24 +0200

    Go-top button + useState. Button appears after scroll

--- src/components/goTopBtn/GoTopBtn.tsx
+++ src/components/goTopBtn/GoTopBtn.tsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
 import {Icon} from "../icon/Icon";
 import {animateScroll as scroll} from "react-scroll";
 
 export const GoTopBtn = () => {
+
+    const [showBtn, setshowBtn] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 200) {
+                setshowBtn(true)
+            } else {
+                setshowBtn(false)
+            }
+        })
+    }, [])
+
+
     return (
-        <StyledGoBtn onClick={() => {scroll.scrollToTop()}}>
-            <Icon iconId={"arrowGoTop"} height={"15"} width={"16"} viewBox={"0 0 16 15"}/>
-        </StyledGoBtn>
+        <>
+            {showBtn && <StyledGoBtn onClick={() => {
+                scroll.scrollToTop()
+            }}>
+                <Icon iconId={"arrowGoTop"} height={"15"} width={"16"} viewBox={"0 0 16 15"}/>
+            </StyledGoBtn>}
+        </>
     );
 };
 
 const StyledGoBtn = styled.button`
-    background-color: rgba(0,0,0,0.3);
+    background-color: rgba(0, 0, 0, 0.3);
     padding: 8px;
     position: fixed;
     right: 30px;
     bottom: 30px
-    
+
 `
